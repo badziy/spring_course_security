b86f0d9ad526ca689ea00bea9226bd9e238cd7f1
Badziev Alan <badjity@yandex.ru>
2022-09-17 12:15:08 +0300

    Initial commit

---
+++ src/main/java/ru/badziy/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package ru.badziy.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
