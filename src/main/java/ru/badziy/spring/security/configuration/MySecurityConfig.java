b7dd4538790fb9d5c0565863c92d994c64fe4fc4
Badziev Alan <badjity@yandex.ru>
2022-09-18 10:58:42 +0300

    Added login with passwords from DB

--- src/main/java/ru/badziy/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/ru/badziy/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.badziy.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        /*
         UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
         auth.inMemoryAuthentication().withUser(userBuilder.username("Alan").password("Alan").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("Soslan").password("Soslan").roles("HR"))
                 .withUser(userBuilder.username("Fatima").password("Fatima").roles("MANAGER", "HR"));
+
+         */
     }
 
     @Override
